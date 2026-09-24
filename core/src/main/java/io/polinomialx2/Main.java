bdd17f6a6326e8ad4374541411a9e6fcf084a978
Rio Sakti Mahendra <rm@tilde.club>
2026-09-24 19:20:02 +0700

    first commit

---
+++ core/src/main/java/io/polinomialx2/Main.java
@@ -0,0 +1,11 @@
+package io.polinomialx2;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
